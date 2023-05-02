dcc6153e1dfe316c8dba2d7826c07d3f88c0ba7e
shc <shinhc0508@naver.com>
2023-05-02 16:35:46 +0900

    first project

---
+++ pybo/forms.py
@@ -0,0 +1,27 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Question, Answer, Comment
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question  # 사용할 모델
+        fields = ['subject', 'content']  # QuestionForm에서 사용할 Question 모델의 속성
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
---
+++ pybo/views/vote_views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
+from django.utils import timezone
+from ..forms import QuestionForm, AnswerForm, CommentForm
+from ..models import Question, Answer, Comment
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+@login_required(login_url='common:login')
+def vote_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user == question.author:
+        messages.error(request, '본인이 작성한 글은 추천할 수 없음')
+    else:
+        question.voter.add(request.user)
+    return redirect('{}#answer_{}'.format(
+        resolve_url('pybo:detail', question_id=question.id), question.id))
+
+@login_required(login_url='common:login')
+def vote_answer(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 글은 추천할 수 없음')
+    else:
+        answer.voter.add(request.user)
+    return redirect('{}#answer_{}'.format(
+        resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
